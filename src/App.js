f41a4c91bc4be05089993f99d806ae9e075901bc
Hugo <hugo.ribaud@epitech.eu>
2021-04-08 15:41:44 +0200

    about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Showcase from './components/Showcase'
-import Projects from './components/Projects'
-import Hire from './components/Hire'
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Showcase from "./components/Showcase";
+import Projects from "./components/Projects";
+import Hire from "./components/Hire";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route path="/" exact>
+      <Route path='/' exact>
         <Showcase />
       </Route>
-      <Route path="/projects">
+      <Route path='/projects'>
         <Projects />
       </Route>
-      <Route path="/hire">
+      <Route path='/hire'>
         <Hire />
+      </Route>
+      <Route path='/about'>
+        <About />
       </Route>
     </Router>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 function About() {
-  return <div></div>;
+  return (
+    <>
+      <div className='about py-20 px-5 flex flex-col items-center-center justify-items-center'>
+        <ReactPlayer
+          className='react-player flex flex-col items-center-center justify-items-center'
+          url='https://youtu.be/MdleK5zHbUQ'
+          width='100%'
+          height='100%'
+          playing='true'
+          loop='true'
+          onReady
+        />
+      </div>
+    </>
+  );
 }
 
 export default About;
